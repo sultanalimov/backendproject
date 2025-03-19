15ad108d48a5a993e9aa861cd773ab22643b371c
sultanalimov <sultanalimov527@gmail.com>
2025-03-19 16:58:36 +0600

    created service

---
+++ src/main/java/org/example/backend/service/EmployeeService.java
@@ -0,0 +1,39 @@
+package org.example.backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import org.example.backend.repo.EmployeeRepo;
+import org.example.backend.model.Employee ;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+    public List<Employee> findAllEmployees(){
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
+    }
+
+
+
+}
+
